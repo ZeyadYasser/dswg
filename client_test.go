3a5e1f8c757ae79086e293979d6283eeadda9b41
zeyady98 <zeyady98@gmail.com>
2020-06-14 19:54:53 +0200

    add tests for Peer operations in Client

--- client_test.go
+++ client_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/vishvananda/netns"
 )
 
+// TODO: Use better error types, and update tests to assert for those types
+
 // Tests that modify the network are preceded by creating
 // a new network namespace to avoid corrupting the current
 // machine network configuration 
@@ -55,6 +57,28 @@ func TestClientAddLinkValidNotEnabled(t *testing.T) {
 	dblink, err := client.db.GetLink(testlink.Name)
 	assert.Nil(err)
 	assert.Equal(testlink, *dblink)
+}
+
+func TestClientAddLinkValidEnabled(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+	
+	testlink := baseLink()
+	testlink.Enable = true
+	
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	netInterface, _ := client.ns.LinkByName(testlink.Name)
+	assert.NotNil(netInterface)
+	assert.Equal(netInterface.Type(), "wireguard")
+	assert.Equal(netInterface.Attrs().Flags & net.FlagUp, net.FlagUp)
 }
 
 func TestClientAddLinkDuplicateDBName(t *testing.T) {
@@ -411,4 +435,511 @@ func TestClientUpdateLinkInvalid(t *testing.T) {
 
 	err = client.UpdateLink(testlink1.Name, testlink2)
 	assert.NotNil(err)
+}
+
+func TestClientAddPeerDuplicateName(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	// Change public key
+	key, _ := ParseKey("CHG+ngJZ2jf+sREdOi/b0D8rTGMbcjgSA854Jn2KbzQ=")
+	testpeer.PublicKey = *key
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.NotNil(err)
+}
+
+func TestClientAddPeerDuplicatePublicKey(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	// Change name
+	testpeer.Name = "new-name"
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.NotNil(err)
+}
+
+func TestClientAddPeerEmptyName(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	testpeer.Name = ""
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.NotNil(err)
+}
+
+func TestClientAddPeerValid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	testpeer.Enable = false
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	dbpeer, err := client.db.GetPeer(testlink.Name, testpeer.Name)
+	assert.Nil(err)
+	assert.Equal(testpeer, *dbpeer)
+}
+
+func TestClientAddPeerEnabledLinkLoaded(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = true
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	testpeer.Enable = true
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	dbpeer, err := client.db.GetPeer(testlink.Name, testpeer.Name)
+	assert.Nil(err)
+	assert.Equal(testpeer, *dbpeer)
+
+	wglink, err := client.wg.Device(testlink.Name)
+	assert.Nil(err)
+	wgpeer := wglink.Peers[0]
+	assert.Equal(testpeer.PublicKey.Key, wgpeer.PublicKey)
+	assert.Equal(testpeer.PresharedKey.Key, wgpeer.PresharedKey)
+	assert.Equal(testpeer.Endpoint.UDPAddr.Port, wgpeer.Endpoint.Port)
+	assert.Equal(testpeer.PersistentKeepalive, int64(wgpeer.PersistentKeepaliveInterval))
+
+	wgallowed := make(map[string]bool)
+	for _, ip := range wgpeer.AllowedIPs {
+		wgallowed[ip.String()] = true
+	}
+	
+	for _, ip := range testpeer.AllowedIPs {
+		assert.True(wgallowed[ip.String()])
+	}
+}
+
+func TestClientRemovePeerValid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	dbpeer, err := client.db.GetPeer(testlink.Name, testpeer.Name)
+	assert.Nil(err)
+	assert.Equal(testpeer, *dbpeer)
+
+	err = client.RemovePeer(testlink.Name, testpeer.Name)
+	assert.Nil(err)
+
+	dbpeer, err = client.db.GetPeer(testlink.Name, testpeer.Name)
+	assert.NotNil(err)
+	assert.Nil(dbpeer)
+}
+
+func TestClientRemovePeerEnabledLinkLoaded(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = true
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	testpeer.Enable = true
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	dbpeer, err := client.db.GetPeer(testlink.Name, testpeer.Name)
+	assert.Nil(err)
+	assert.Equal(testpeer, *dbpeer)
+
+	wglink, err := client.wg.Device(testlink.Name)
+	assert.Nil(err)
+	assert.Equal(len(wglink.Peers), 1)
+
+	err = client.RemovePeer(testlink.Name, testpeer.Name)
+	assert.Nil(err)
+
+	wglink, err = client.wg.Device(testlink.Name)
+	assert.Nil(err)
+	assert.Equal(len(wglink.Peers), 0)
+}
+
+func TestClientRemovePeerNotExist(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	err = client.RemovePeer(testlink.Name, "no-peero")
+	assert.NotNil(err)
+}
+
+func TestClientActivatePeerLinkNotLoaded(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	testpeer.Enable = false
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	err = client.ActivatePeer(testlink.Name, testpeer.Name)
+	assert.NotNil(err)
+}
+
+func TestClientActivatePeerNotExist(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	err = client.ActivatePeer(testlink.Name, "no-peer")
+	assert.NotNil(err)
+}
+
+func TestClientActivatePeerValid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = true
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	testpeer.Enable = false
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	wglink, _ := client.wg.Device(testlink.Name)
+	assert.Equal(len(wglink.Peers), 0)
+
+	err = client.ActivatePeer(testlink.Name, testpeer.Name)
+	assert.Nil(err)
+
+	dbpeer, _ := client.db.GetPeer(testlink.Name, testpeer.Name)
+	assert.True(dbpeer.Enable)
+
+	wglink, _ = client.wg.Device(testlink.Name)
+	assert.Equal(len(wglink.Peers), 1)
+}
+
+func TestClientDeactivatePeerLinkNotLoaded(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	testpeer.Enable = false
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	err = client.DeactivatePeer(testlink.Name, testpeer.Name)
+	assert.NotNil(err)
+}
+
+func TestClientDeactivatePeerNotExist(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	err = client.DeactivatePeer(testlink.Name, "no-peer")
+	assert.NotNil(err)
+}
+
+func TestClientDeactivatePeerValid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = true
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	testpeer.Enable = true
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	wglink, _ := client.wg.Device(testlink.Name)
+	assert.Equal(len(wglink.Peers), 1)
+
+	err = client.DeactivatePeer(testlink.Name, testpeer.Name)
+	assert.Nil(err)
+
+	dbpeer, _ := client.db.GetPeer(testlink.Name, testpeer.Name)
+	assert.False(dbpeer.Enable)
+
+	wglink, _ = client.wg.Device(testlink.Name)
+	assert.Equal(len(wglink.Peers), 0)
+}
+
+func TestClientUpdatePeerInvalid(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer1 := basePeer()
+	testpeer1.Enable = false
+	err = client.AddPeer(testlink.Name, testpeer1)
+	assert.Nil(err)
+
+	testpeer2 := basePeer()
+	testpeer2.Name = ""
+	err = client.UpdatePeer(testlink.Name, testpeer1.Name, testpeer2)
+	assert.NotNil(err)
+
+	dbpeer, _ := client.db.GetPeer(testlink.Name, testpeer1.Name)
+	assert.Equal(testpeer1, *dbpeer)
+}
+
+func TestClientUpdatePeerDuplicateName(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer1 := basePeer()
+	testpeer1.Enable = false
+	testpeer1.Name = "peer1"
+	err = client.AddPeer(testlink.Name, testpeer1)
+	assert.Nil(err)
+
+	testpeer2 := basePeer()
+	testpeer2.Name = "peer2"
+	testpeer1.Enable = false
+	// Change public key
+	key, _ := ParseKey("CHG+ngJZ2jf+sREdOi/b0D8rTGMbcjgSA854Jn2KbzQ=")
+	testpeer2.PublicKey = *key
+	err = client.AddPeer(testlink.Name, testpeer2)
+	assert.Nil(err)
+
+	err = client.UpdatePeer(testlink.Name, testpeer1.Name, testpeer2)
+	assert.NotNil(err)
+
+	dbpeer, _ := client.db.GetPeer(testlink.Name, testpeer1.Name)
+	assert.Equal(testpeer1, *dbpeer)
+
+	dbpeer, _ = client.db.GetPeer(testlink.Name, testpeer2.Name)
+	assert.Equal(testpeer2, *dbpeer)
+}
+
+func TestClientUpdatePeerLinkLoadedEnable(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = true
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	testpeer.Enable = false
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	wglink, _ := client.wg.Device(testlink.Name)
+	assert.Equal(len(wglink.Peers), 0)
+
+	testpeer.Enable = true
+	err = client.UpdatePeer(testlink.Name, testpeer.Name, testpeer)
+	assert.Nil(err)
+
+	dbpeer, _ := client.db.GetPeer(testlink.Name, testpeer.Name)
+	assert.Equal(testpeer, *dbpeer)
+
+	wglink, _ = client.wg.Device(testlink.Name)
+	assert.Equal(len(wglink.Peers), 1)
+}
+
+func TestClientUpdatePeerLinkNotLoaded(t *testing.T) {
+	assert := assert.New(t)
+
+	// Create a new network namespace 
+	netns, _ := netns.New()
+	defer netns.Close()
+
+	client := baseClient()
+	defer client.Close()
+
+	testlink := baseLink()
+	testlink.Enable = false
+	err := client.AddLink(testlink)
+	assert.Nil(err)
+
+	testpeer := basePeer()
+	testpeer.Enable = false
+	err = client.AddPeer(testlink.Name, testpeer)
+	assert.Nil(err)
+
+	err = client.UpdatePeer(testlink.Name, testpeer.Name, testpeer)
+	assert.Nil(err)
+
+	dbpeer, _ := client.db.GetPeer(testlink.Name, testpeer.Name)
+	assert.Equal(testpeer, *dbpeer)
 }
